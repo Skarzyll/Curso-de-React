cc7715abfad99c1ccbb85d96f060578aea01c2e7
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-09-20 11:42:52 -0300

    state lift

--- vite-r-1/src/App.tsx
+++ vite-r-1/src/App.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import { Hel } from './components/Hello'
+import Form from './components/Form'
 
 
 export default function App() {
 
-  const some = (a,b) => {
+  const soma = (a,b) => {
     return a+b
   }
 
+  const [nome, setarNome] = useState('Nome exemplo')
+
   return (
-    <div className="w-screen h-screen bg-zinc-700 text-white p-3">
-      helo world {some(1,2)}
+    <div className="w-screen h-screen bg-zinc-700 p-3">
+      helo world {soma(1,2)}
       <Hel idade={20} />
+      <Form title={"NOME"} setarNome={setarNome} nome={nome} />
     </div>
   )
 }
---
+++ vite-r-1/src/components/Form.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Form({ title, setarNome, nome }) {
+
+
+    return (
+        <>
+            <h3>{title}</h3>
+            <form>
+                <label>
+                    Seu nome: <input type="text" placeholder="Nome!" onChange={(e) => setarNome(e.target.value)} />
+                </label>
+            </form>
+            <p>{nome}</p>
+        </>
+    )
+}
