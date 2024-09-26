027ecf645dd2dc5735897acc15943aea27184a8c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-09-26 11:37:45 -0300

    React Icons

--- vite-r-1/src/pages/NotFoundPage.tsx
+++ vite-r-1/src/pages/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import { useRouteError } from "react-router-dom";
+import { FaCrosshairs, FaFacebook } from 'react-icons/fa6'
 
 export default function NotFoundPage(){
     const error = useRouteError()
@@ -13,6 +14,8 @@ export default function NotFoundPage(){
             <header>
                 404
                 <strong>NOT FOUND PAGE</strong>
+                <FaCrosshairs className="text-cyan-700 h-9 w-9" />
+                <FaFacebook />
             </header>
         </>
     )
