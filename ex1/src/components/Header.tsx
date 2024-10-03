7cbf8219d0ba05ca39a5180a7d1010f689a3b0af
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-10-03 11:53:25 -0300

    update ex1

--- ex1/src/components/Header.tsx
+++ ex1/src/components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from "./NavBar";
 
 export default function Header() {
     return(
-        <header className="">
+        <header className="h-24 w-full justify-around">
             <img src="" alt="" />
             <NavBar />
         </header>
--- ex1/src/components/NavBar.tsx
+++ ex1/src/components/NavBar.tsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 export default function NevBar() {
     return(
         <>
-            <ol className="h-5 w-5 flex items-stretch justify-between">
-                <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/pages/Contato.tsx"}>Contato</Link></li>
-                <li><Link to={"/pages/Empresa.tsx"}>Empresa</Link></li>
-                <li><Link to={"/pages/NovoProjeto.tsx"}>Novo Projeto</Link></li>
-                </ol>
+            <ol className="flex h-full w-2/4 items-stretch justify-between m-0 p-0">
+                <li className="p-4"><Link to={"/"}>Home</Link></li>
+                <li className="p-4"><Link to={"/pages/Contato.tsx"}>Contato</Link></li>
+                <li className="p-4"><Link to={"/pages/Empresa.tsx"}>Empresa</Link></li>
+                <li className="p-4"><Link to={"/pages/NovoProjeto.tsx"}>Novo Projeto</Link></li>
+            </ol>
         </>
     )
 }
