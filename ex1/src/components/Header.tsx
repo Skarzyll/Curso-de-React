aa0fffbb80fbd1700b6ee22fc6e9dedf54898a1c
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-10-04 11:39:28 -0300

    update ex1

--- ex1/src/components/Header.tsx
+++ ex1/src/components/Header.tsx
@@ -1,9 +1,10 @@
 import NavBar from "./NavBar";
+import logo from "../images/logo512.png"
 
 export default function Header() {
     return(
-        <header className="h-24 w-full justify-around">
-            <img src="" alt="" />
+        <header className="flex h-24 w-full justify-between items-center content-center">
+            <img src={logo} alt="logo" className="h-full" />
             <NavBar />
         </header>
     )
--- ex1/src/components/NavBar.tsx
+++ ex1/src/components/NavBar.tsx
@@ -3,11 +3,10 @@ import { Link } from "react-router-dom";
 export default function NevBar() {
     return(
         <>
-            <ol className="flex h-full w-2/4 items-stretch justify-between m-0 p-0">
-                <li className="p-4"><Link to={"/"}>Home</Link></li>
-                <li className="p-4"><Link to={"/pages/Contato.tsx"}>Contato</Link></li>
-                <li className="p-4"><Link to={"/pages/Empresa.tsx"}>Empresa</Link></li>
-                <li className="p-4"><Link to={"/pages/NovoProjeto.tsx"}>Novo Projeto</Link></li>
+            <ol className="flex h-full w-2/4 items-center justify-evenly">
+                <li><Link to={"/"}  className="pt-3 pb-3 pr-5 pl-5">Home</Link></li>
+                <li><Link to={"/pages/Contato.tsx"}  className="pt-3 pb-3 pr-5 pl-5">Contato</Link></li>
+                <li><Link to={"/pages/Empresa.tsx"}  className="pt-3 pb-3 pr-5 pl-5">Empresa</Link></li>
             </ol>
         </>
     )
