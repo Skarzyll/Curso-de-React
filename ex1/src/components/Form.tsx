e1895d1da600838607b0aa0ead99d24ff7a4ab3b
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-10-18 11:51:51 -0300

    update ex1

--- ex1/src/components/Form.tsx
+++ ex1/src/components/Form.tsx
@@ -1,9 +1,18 @@
-
-// eslint-disable-next-line react-refresh/only-export-components
-export default function() {
+export default function Form() {
     return(
-        <form action="" method="post">
-            
+        <form action="" method="post" className="border border-black">
+            <label className="">
+                nome: 
+                <input type="text" name="" id="" placeholder="Insira o nome do projeto." />
+            </label><br />
+            <label>
+                Orçamento: 
+                <input type="text" name="" id="" placeholder="Insira o orçamento do projeto." />
+            </label><br />
+            <select name="" id="">
+                <option value="a">a</option>
+                <option value="">s</option>
+            </select>
         </form>
     )
 }    
--- ex1/src/pages/NovoProjeto.tsx
+++ ex1/src/pages/NovoProjeto.tsx
@@ -1,14 +1,15 @@
 import Footer from "../components/Footer";
+import Form from "../components/Form";
 import Header from "../components/Header";
 
 export default function NovoProjeto() {
     return(
-        <div className="h-screen w-screen">
+        <div className="h-screen w-screen text-center">
             <Header />
             <div className="min-h-min-70 p-2">
-                <h1>Criar projeto</h1>
-                <p>Crie seu projeto para depois adicionar os serviços.</p>
-                <p>form</p>
+                <h1 className="text-4xl font-bold m-3">Criar projeto</h1>
+                <p className="m-3">Crie seu projeto para depois adicionar os serviços.</p>
+                <Form />
             </div>
             <Footer />
         </div>
